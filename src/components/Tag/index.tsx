b67bcfe3cdbe6604ae89063c94f50fbc7608c743
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2023-06-14 16:20:40 -0300

    primeira parte concluida

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,9 @@
+import { TagContainer } from './styles'
+
+export type Props = {
+  size?: 'small' | 'big'
+  children: string
+}
+export const Tag = ({ children, size = 'small' }: Props) => (
+  <TagContainer size={size}>{children}</TagContainer>
+)
